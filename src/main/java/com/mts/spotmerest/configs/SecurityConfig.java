d09f01a2db9155c41e11d828ae5d5ba18b1353c5
chihabam <chihabam@gmail.com>
2025-11-17 20:21:26 -0600

    Refactor: Update CORS configuration to allow all origins and enhance local environment properties

--- src/main/java/com/mts/spotmerest/configs/SecurityConfig.java
+++ src/main/java/com/mts/spotmerest/configs/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:8100","http://localhost:8083",gateWayURL,"http://localhost:8081","http://localhost:5173","http://localhost:3000",originUrl,nodeURL,nodeURL+":[*]","https://rest.spot-me-app.com/",uiURL,"https://ui.spot-me-app.com/"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
         corsConfiguration.setAllowedMethods(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
